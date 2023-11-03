33f31a3627a5b2e7a133bf32112a9c3df12a9be8
deianearra.septi <deianearras@gmail.com>
2023-11-03 08:47:48 +0700

    Fixing

--- talenesia/src/main.jsx
+++ talenesia/src/main.jsx
@@ -1,38 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './styles/styles.css';
-// import './index.css'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
-
-// main.jsx
-
-// import { createRoot } from 'react-dom';
-// import App from './App';
-// import './src/styles/styles.css';
-
-// const root = createRoot(document.getElementById('app'));
-// root.render(<App />);
-
-// src/main.tsx
-// src/main.tsx
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './styles/index.css';
-// import Login from './views/Login';
-
-// const root = document.getElementById ('app');
-
-// if (root) {
-//   const reactRoot = ReactDOM.createRoot (root);
-//   reactRoot.render (
-//     <React.StrictMode>
-//       <Login />
-//     </React.StrictMode>
-//   );
-// }
--- talenesia/vite.config.js
+++ talenesia/vite.config.js
@@ -1,16 +1,7 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue';
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), react()],
-  server: {
-    fs: {
-      strict: false
-    }
-  },
-  optimizeDeps: {
-    include: ['@vue/compiler-sfc']
-  }
+  plugins: [react()],
 })
