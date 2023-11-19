3c671301dd74775810fc96f467cc4e7198c99b8c
Deni Fahrony <denaaay@192.168.0.116>
2023-11-19 17:20:58 +0700

    add teacher page

---
+++ talenesia/src/components/molecules/TeacherContent.jsx
@@ -0,0 +1,170 @@
+import ProfilePict from '../../assets/profile.png'
+
+function TeacherContent() {
+    return(
+        <div className="w-full flex flex-col gap-2">
+            <div className="w-full flex flex-col gap-2">
+                <p className="font-semibold text-lg text-[#143B75]">Mentor Coding</p>
+                <div className="overflow-x-auto">
+                    <div className="w-max flex gap-4">
+                        <a href="" className='mb-6'>
+                            <div className="w-[150px] h-[200px] flex flex-col justify-start items-center border shadow-md rounded-md">
+                                <div className="w-full h-[40%] flex justify-center items-center h-3/6 bg-gradient-to-b from-[#143B75] to-white rounded-md">
+                                    <div className="w-[50px] h-[50px] rounded-[50%]">
+                                        <img src={ProfilePict} alt="Profile Picture" className='w-full h-full'/>
+                                    </div>
+                                </div>
+                                <div className='flex-1 w-[90%] flex flex-col justify-center items-center'>
+                                    <p className='font-medium text-sm text-[#143B75]mb-2'>Mentor Dummy</p>
+                                    <p className='text-[8px] text-justify overflow-hidden line-clamp-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae vero sapiente mollitia soluta nemo quisquam velit magni ratione ab placeat ea consequatur tenetur, eius, molestias tempora nostrum ut aperiam maiores!</p>
+                                </div>
+                            </div>
+                        </a>
+                        <a href="" className='mb-6'>
+                            <div className="w-[150px] h-[200px] flex flex-col justify-start items-center border shadow-md rounded-md">
+                                <div className="w-full h-[40%] flex justify-center items-center h-3/6 bg-gradient-to-b from-[#143B75] to-white rounded-md">
+                                    <div className="w-[50px] h-[50px] rounded-[50%]">
+                                        <img src={ProfilePict} alt="Profile Picture" className='w-full h-full'/>
+                                    </div>
+                                </div>
+                                <div className='flex-1 w-[90%] flex flex-col justify-center items-center'>
+                                    <p className='font-medium text-sm text-[#143B75]mb-2'>Mentor Dummy</p>
+                                    <p className='text-[8px] text-justify overflow-hidden line-clamp-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae vero sapiente mollitia soluta nemo quisquam velit magni ratione ab placeat ea consequatur tenetur, eius, molestias tempora nostrum ut aperiam maiores!</p>
+                                </div>
+                            </div>
+                        </a><a href="" className='mb-6'>
+                            <div className="w-[150px] h-[200px] flex flex-col justify-start items-center border shadow-md rounded-md">
+                                <div className="w-full h-[40%] flex justify-center items-center h-3/6 bg-gradient-to-b from-[#143B75] to-white rounded-md">
+                                    <div className="w-[50px] h-[50px] rounded-[50%]">
+                                        <img src={ProfilePict} alt="Profile Picture" className='w-full h-full'/>
+                                    </div>
+                                </div>
+                                <div className='flex-1 w-[90%] flex flex-col justify-center items-center'>
+                                    <p className='font-medium text-sm text-[#143B75]mb-2'>Mentor Dummy</p>
+                                    <p className='text-[8px] text-justify overflow-hidden line-clamp-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae vero sapiente mollitia soluta nemo quisquam velit magni ratione ab placeat ea consequatur tenetur, eius, molestias tempora nostrum ut aperiam maiores!</p>
+                                </div>
+                            </div>
+                        </a><a href="" className='mb-6'>
+                            <div className="w-[150px] h-[200px] flex flex-col justify-start items-center border shadow-md rounded-md">
+                                <div className="w-full h-[40%] flex justify-center items-center h-3/6 bg-gradient-to-b from-[#143B75] to-white rounded-md">
+                                    <div className="w-[50px] h-[50px] rounded-[50%]">
+                                        <img src={ProfilePict} alt="Profile Picture" className='w-full h-full'/>
+                                    </div>
+                                </div>
+                                <div className='flex-1 w-[90%] flex flex-col justify-center items-center'>
+                                    <p className='font-medium text-sm text-[#143B75]mb-2'>Mentor Dummy</p>
+                                    <p className='text-[8px] text-justify overflow-hidden line-clamp-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae vero sapiente mollitia soluta nemo quisquam velit magni ratione ab placeat ea consequatur tenetur, eius, molestias tempora nostrum ut aperiam maiores!</p>
+                                </div>
+                            </div>
+                        </a><a href="" className='mb-6'>
+                            <div className="w-[150px] h-[200px] flex flex-col justify-start items-center border shadow-md rounded-md">
+                                <div className="w-full h-[40%] flex justify-center items-center h-3/6 bg-gradient-to-b from-[#143B75] to-white rounded-md">
+                                    <div className="w-[50px] h-[50px] rounded-[50%]">
+                                        <img src={ProfilePict} alt="Profile Picture" className='w-full h-full'/>
+                                    </div>
+                                </div>
+                                <div className='flex-1 w-[90%] flex flex-col justify-center items-center'>
+                                    <p className='font-medium text-sm text-[#143B75]mb-2'>Mentor Dummy</p>
+                                    <p className='text-[8px] text-justify overflow-hidden line-clamp-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae vero sapiente mollitia soluta nemo quisquam velit magni ratione ab placeat ea consequatur tenetur, eius, molestias tempora nostrum ut aperiam maiores!</p>
+                                </div>
+                            </div>
+                        </a><a href="" className='mb-6'>
+                            <div className="w-[150px] h-[200px] flex flex-col justify-start items-center border shadow-md rounded-md">
+                                <div className="w-full h-[40%] flex justify-center items-center h-3/6 bg-gradient-to-b from-[#143B75] to-white rounded-md">
+                                    <div className="w-[50px] h-[50px] rounded-[50%]">
+                                        <img src={ProfilePict} alt="Profile Picture" className='w-full h-full'/>
+                                    </div>
+                                </div>
+                                <div className='flex-1 w-[90%] flex flex-col justify-center items-center'>
+                                    <p className='font-medium text-sm text-[#143B75]mb-2'>Mentor Dummy</p>
+                                    <p className='text-[8px] text-justify overflow-hidden line-clamp-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae vero sapiente mollitia soluta nemo quisquam velit magni ratione ab placeat ea consequatur tenetur, eius, molestias tempora nostrum ut aperiam maiores!</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            <p className="font-semibold text-lg text-[#143B75]">Mentor Coding</p>
+                <div className="overflow-x-auto">
+                    <div className="w-max flex gap-4">
+                        <a href="" className='mb-6'>
+                            <div className="w-[150px] h-[200px] flex flex-col justify-start items-center border shadow-md rounded-md">
+                                <div className="w-full h-[40%] flex justify-center items-center h-3/6 bg-gradient-to-b from-[#143B75] to-white rounded-md">
+                                    <div className="w-[50px] h-[50px] rounded-[50%]">
+                                        <img src={ProfilePict} alt="Profile Picture" className='w-full h-full'/>
+                                    </div>
+                                </div>
+                                <div className='flex-1 w-[90%] flex flex-col justify-center items-center'>
+                                    <p className='font-medium text-sm text-[#143B75]mb-2'>Mentor Dummy</p>
+                                    <p className='text-[8px] text-justify overflow-hidden line-clamp-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae vero sapiente mollitia soluta nemo quisquam velit magni ratione ab placeat ea consequatur tenetur, eius, molestias tempora nostrum ut aperiam maiores!</p>
+                                </div>
+                            </div>
+                        </a>
+                        <a href="" className='mb-6'>
+                            <div className="w-[150px] h-[200px] flex flex-col justify-start items-center border shadow-md rounded-md">
+                                <div className="w-full h-[40%] flex justify-center items-center h-3/6 bg-gradient-to-b from-[#143B75] to-white rounded-md">
+                                    <div className="w-[50px] h-[50px] rounded-[50%]">
+                                        <img src={ProfilePict} alt="Profile Picture" className='w-full h-full'/>
+                                    </div>
+                                </div>
+                                <div className='flex-1 w-[90%] flex flex-col justify-center items-center'>
+                                    <p className='font-medium text-sm text-[#143B75]mb-2'>Mentor Dummy</p>
+                                    <p className='text-[8px] text-justify overflow-hidden line-clamp-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae vero sapiente mollitia soluta nemo quisquam velit magni ratione ab placeat ea consequatur tenetur, eius, molestias tempora nostrum ut aperiam maiores!</p>
+                                </div>
+                            </div>
+                        </a><a href="" className='mb-6'>
+                            <div className="w-[150px] h-[200px] flex flex-col justify-start items-center border shadow-md rounded-md">
+                                <div className="w-full h-[40%] flex justify-center items-center h-3/6 bg-gradient-to-b from-[#143B75] to-white rounded-md">
+                                    <div className="w-[50px] h-[50px] rounded-[50%]">
+                                        <img src={ProfilePict} alt="Profile Picture" className='w-full h-full'/>
+                                    </div>
+                                </div>
+                                <div className='flex-1 w-[90%] flex flex-col justify-center items-center'>
+                                    <p className='font-medium text-sm text-[#143B75]mb-2'>Mentor Dummy</p>
+                                    <p className='text-[8px] text-justify overflow-hidden line-clamp-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae vero sapiente mollitia soluta nemo quisquam velit magni ratione ab placeat ea consequatur tenetur, eius, molestias tempora nostrum ut aperiam maiores!</p>
+                                </div>
+                            </div>
+                        </a><a href="" className='mb-6'>
+                            <div className="w-[150px] h-[200px] flex flex-col justify-start items-center border shadow-md rounded-md">
+                                <div className="w-full h-[40%] flex justify-center items-center h-3/6 bg-gradient-to-b from-[#143B75] to-white rounded-md">
+                                    <div className="w-[50px] h-[50px] rounded-[50%]">
+                                        <img src={ProfilePict} alt="Profile Picture" className='w-full h-full'/>
+                                    </div>
+                                </div>
+                                <div className='flex-1 w-[90%] flex flex-col justify-center items-center'>
+                                    <p className='font-medium text-sm text-[#143B75]mb-2'>Mentor Dummy</p>
+                                    <p className='text-[8px] text-justify overflow-hidden line-clamp-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae vero sapiente mollitia soluta nemo quisquam velit magni ratione ab placeat ea consequatur tenetur, eius, molestias tempora nostrum ut aperiam maiores!</p>
+                                </div>
+                            </div>
+                        </a><a href="" className='mb-6'>
+                            <div className="w-[150px] h-[200px] flex flex-col justify-start items-center border shadow-md rounded-md">
+                                <div className="w-full h-[40%] flex justify-center items-center h-3/6 bg-gradient-to-b from-[#143B75] to-white rounded-md">
+                                    <div className="w-[50px] h-[50px] rounded-[50%]">
+                                        <img src={ProfilePict} alt="Profile Picture" className='w-full h-full'/>
+                                    </div>
+                                </div>
+                                <div className='flex-1 w-[90%] flex flex-col justify-center items-center'>
+                                    <p className='font-medium text-sm text-[#143B75]mb-2'>Mentor Dummy</p>
+                                    <p className='text-[8px] text-justify overflow-hidden line-clamp-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae vero sapiente mollitia soluta nemo quisquam velit magni ratione ab placeat ea consequatur tenetur, eius, molestias tempora nostrum ut aperiam maiores!</p>
+                                </div>
+                            </div>
+                        </a><a href="" className='mb-6'>
+                            <div className="w-[150px] h-[200px] flex flex-col justify-start items-center border shadow-md rounded-md">
+                                <div className="w-full h-[40%] flex justify-center items-center h-3/6 bg-gradient-to-b from-[#143B75] to-white rounded-md">
+                                    <div className="w-[50px] h-[50px] rounded-[50%]">
+                                        <img src={ProfilePict} alt="Profile Picture" className='w-full h-full'/>
+                                    </div>
+                                </div>
+                                <div className='flex-1 w-[90%] flex flex-col justify-center items-center'>
+                                    <p className='font-medium text-sm text-[#143B75]mb-2'>Mentor Dummy</p>
+                                    <p className='text-[8px] text-justify overflow-hidden line-clamp-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae vero sapiente mollitia soluta nemo quisquam velit magni ratione ab placeat ea consequatur tenetur, eius, molestias tempora nostrum ut aperiam maiores!</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default TeacherContent;
